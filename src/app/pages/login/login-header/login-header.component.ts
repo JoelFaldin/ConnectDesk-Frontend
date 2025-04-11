dff6373f84d5b126276149ed23f932fd92c95e39
JoelFaldin <joelfaldin@gmail.com>
2025-04-11 12:23:40 -0400

    feat: added login header content

---
+++ src/app/pages/login/login-header/login-header.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'login-header',
+  imports: [MatIconModule],
+  templateUrl: './login-header.component.html',
+})
+export class LoginHeaderComponent { }
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { LoginHeaderComponent } from "./login-header/login-header.component";
+import { LoginFormComponent } from "./login-form/login-form.component";
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [LoginHeaderComponent, LoginFormComponent],
   templateUrl: './login.component.html',
 })
 export default class LoginComponent { }
