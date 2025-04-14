b8693046190fb3753934eff1091a8ac031c2f038
JoelFaldin <joelfaldin@gmail.com>
2025-04-14 13:01:52 -0400

    feat: finishing register page styles

---
+++ src/app/pages/register/register-form/register-form.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'register-form',
+  imports: [RouterLink],
+  templateUrl: './register-form.component.html',
+})
+export class RegisterFormComponent { }
--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RegisterHeaderComponent } from "./register-header/register-header.component";
+import { RegisterFormComponent } from "./register-form/register-form.component";
 
 @Component({
   selector: 'app-register',
-  imports: [RegisterHeaderComponent],
+  imports: [RegisterHeaderComponent, RegisterFormComponent],
   templateUrl: './register.component.html',
 })
 export default class RegisterComponent { }
