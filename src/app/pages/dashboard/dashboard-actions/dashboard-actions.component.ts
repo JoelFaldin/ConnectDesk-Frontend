dd2309ed6c7d7ed5d83d71f95110df12e8a8ad22
JoelF <joelfaldin@gmail.com>
2025-04-27 21:25:10 -0400

    feat: implementing log data download functionality in quick actions

--- src/app/pages/dashboard/dashboard-actions/dashboard-actions.component.ts
+++ src/app/pages/dashboard/dashboard-actions/dashboard-actions.component.ts
@@ -30,6 +30,19 @@ export class DashboardActionsComponent {
     })
   }
 
+  handleDownloadLogsData() {
+    this.excelService.downloadLogsData().subscribe(blob => {
+      const url = window.URL.createObjectURL(blob);
+      const a = document.createElement('a');
+
+      a.href = url;
+      a.download = 'logsdata.xlsx';
+      a.click();
+
+      window.URL.revokeObjectURL(url);
+    })
+  }
+
   toggleVisibility(value: boolean) {
     this.visible.set(value);
   }
