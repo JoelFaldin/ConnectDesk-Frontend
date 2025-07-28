cd4e549900530c2ed8cce2d2c0a8e536e9bd0484
JoelF <joelfaldin@gmail.com>
2025-07-27 23:55:25 -0400

    feat: adding basic markup for reset password modal

--- src/app/pages/users/users-table/users-actions/reset-modal/reset-modal.component.ts
+++ src/app/pages/users/users-table/users-actions/reset-modal/reset-modal.component.ts
@@ -1,10 +1,35 @@
-import { Component, Input } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+
+import { Component, inject, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+import { UserService } from '@services/user.service';
+import { User } from '@interfaces/user.interface';
 
 @Component({
   selector: 'reset-modal',
-  imports: [],
+  imports: [ReactiveFormsModule, ButtonModule],
   templateUrl: './reset-modal.component.html',
 })
-export class ResetModalComponent {
+export class ResetModalComponent implements OnInit {
+  userService = inject(UserService);
+
   @Input() userRut: any;
+  user: User | undefined;
+
+  form = new FormGroup({
+    newPassword: new FormControl('', Validators.required),
+    repeatPassword: new FormControl('', Validators.required),
+  })
+
+  ngOnInit(): void {
+    this.userService.getSingleUser(this.userRut).subscribe(user => {
+      this.user = user;
+    })
+  }
+
+  handleResetPassword() {
+    console.log('aaa');
+    console.log(this.form.value)
+  }
 }
