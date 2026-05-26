34c941ebac691fed23fc6b8a185a5ff154ddde96
JoelFaldin <joelfaldin@gmail.com>
2026-05-25 23:47:03 -0400

    refactor: fix problem on table logs

--- src/app/pages/logs/table-logs/table-logs.component.ts
+++ src/app/pages/logs/table-logs/table-logs.component.ts
@@ -1,18 +1,18 @@
-import { MatIconModule } from '@angular/material/icon';
-import { TableModule } from 'primeng/table';
+import { MatIconModule } from "@angular/material/icon";
+import { TableModule } from "primeng/table";
 
-import { Component, inject } from '@angular/core';
-import { DatePipe } from '@angular/common';
+import { Component, inject } from "@angular/core";
+import { DatePipe } from "@angular/common";
 
-import { TableSelectComponent } from './table-select/table-select.component';
-import { LogsDataResponse, LogsInterface } from '@interfaces/logs.interface';
-import { ToastService } from '@services/toast.service';
-import { LogsService } from '@services/logs.service';
+import { TableSelectComponent } from "./table-select/table-select.component";
+import { LogsDataResponse, LogsInterface } from "@interfaces/logs.interface";
+import { ToastService } from "@services/toast.service";
+import { LogsService } from "@services/logs.service";
 
 @Component({
-  selector: 'table-logs',
+  selector: "table-logs",
   imports: [TableModule, MatIconModule, TableSelectComponent, DatePipe],
-  templateUrl: './table-logs.component.html',
+  templateUrl: "./table-logs.component.html",
 })
 export class TableLogsComponent {
   logsService = inject(LogsService);
@@ -20,12 +20,12 @@ export class TableLogsComponent {
 
   dataSource: LogsInterface[] = [];
 
-  displayedColumns = ['Timestamp', 'Endpoint', 'Method', 'Status Code'];
+  displayedColumns = ["Timestamp", "Endpoint", "Method", "Status Code"];
 
   ngOnInit() {
-    this.logsService.logsData$.subscribe(logs => {
+    this.logsService.logsData$.subscribe((logs) => {
       this.dataSource = logs;
-    })
+    });
 
     this.fetchLogs();
   }
@@ -39,30 +39,31 @@ export class TableLogsComponent {
         }
 
         this.dataSource = (res.content ?? []).map((log: LogsInterface) => {
-          const newEndpoint = log.endpoint.slice(4);
-
           return {
             ...log,
             statusCode: Number(log.statusCode),
-            endpoint: newEndpoint,
             date: new Date(log.date),
-          }
-        })
+          };
+        });
 
         this.logsService.setLogData(this.dataSource);
 
         this.logsService.setPaginationData({
           page: res.page!,
           pageSize: res.pageSize!,
-          total: res.total!
+          total: res.total!,
         });
 
-        this.toast.success('Success', 'Logs obtained!');
+        this.toast.success("Success", "Logs obtained!");
       },
       error: (error) => {
-        this.toast.error('Error', error.error.message ?? 'There was a problem with the server, try again later.');
+        this.toast.error(
+          "Error",
+          error.error.message ??
+            "There was a problem with the server, try again later.",
+        );
         // console.error(error);
-      }
-    })
+      },
+    });
   }
 }
